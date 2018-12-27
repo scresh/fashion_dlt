5cf4eca2f6ed47931386bf12b615cc602d8d8323
scresh <screshowski@gmail.com>
2018-12-27 21:05:29 +0100

    Change consensus algorithm to PoET

--- fashion-client/web_frontend/src/App.js
+++ fashion-client/web_frontend/src/App.js
@@ -1,25 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router , Link} from 'react-router-dom';
+import { Layout, Menu } from 'antd';
+import BaseRouter from './routes';
+const { Header, Content, Footer} = Layout;
+
+
+const AppLayout = (props) => {
+    return (
+        <Layout className="layout">
+            <Header>
+                <div className="logo" />
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={['2']}
+                    style={{ lineHeight: '64px' }}
+                >
+                    <Menu.Item key="home"><Link to="/">Home</Link></Menu.Item>
+
+                </Menu>
+            </Header>
+            <Content style={{ padding: '0 50px' }}>
+                <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+                    {props.children}
+                </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>
+                Ant Design ©2018 Created by Ant UED
+            </Footer>
+        </Layout>
+    );
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <Router>
+              <AppLayout>
+                <BaseRouter />
+              </AppLayout>
+          </Router>
       </div>
     );
   }
