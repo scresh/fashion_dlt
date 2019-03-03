82f307e93bc42f6a797b9cd5228931f2a7a82180
scresh <screshowski@gmail.com>
2019-03-03 15:14:44 +0100

    Fix start script

--- start.sh
+++ start.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 if ! (dpkg -l | grep docker-compose > /dev/null); then
-  sudo apt-get update
-  sudo apt-get install \
+  yes | sudo apt-get update
+  yes | sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  sudo apt-key fingerprint 0EBFCD88
-  sudo add-apt-repository \
+  yes | curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  yes | sudo apt-key fingerprint 0EBFCD88
+  yes | sudo add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) \
     stable"
 
-  sudo apt-get update
-  sudo apt-get install docker-ce
+  yes | sudo apt-get update
+  yes | sudo apt-get install docker-ce
 
-  sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  sudo chmod +x /usr/local/bin/docker-compose
+  yes | sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  yes | sudo chmod +x /usr/local/bin/docker-compose
 
-  sudo groupadd docker
-  sudo usermod -aG docker $USER
+  yes | sudo groupadd docker
+  yes | sudo usermod -aG docker $USER
 fi
-docker-compose -f docker-compose.yaml down > /dev/null
-yes | docker volume prune > /dev/null
-docker-compose -f docker-compose.yaml up
+sudo docker-compose -f docker-compose.yaml down > /dev/null
+yes | sudo docker volume prune > /dev/null
+yes | sudo docker-compose -f docker-compose.yaml up
